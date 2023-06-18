6655acbadd32ba3630f7058d840f56f6814190c7
EhsaasChaudhary <21010101028@darshan.ac.in>
2023-06-18 19:10:10 +0530

    navbar

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -16,9 +16,9 @@ module.exports = {
   },
   plugins: [require("daisyui")],
 
-  daisyui: {
-    themes: false, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: "dark", // name of one of the included themes for dark mode
+    daisyui: {
+    themes: ["forest", "dark", "cmyk"],
+    darkTheme: "forest", // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
