056e34a1a5b3c73272223a3d7caab36b37e76360
soham901 <sagathiyasoham12345@gmail.com>
2023-06-18 09:29:18 +0530

    Implemented more types in types.d.ts

--- frontend/types.d.ts
+++ frontend/types.d.ts
@@ -2,3 +2,25 @@ type Test = {
     "msg": string
 }
 
+type User = {
+    "id": int,
+    "fullname": string,
+}
+
+type BaseCode = {
+    "id": int,
+    "title": string,
+    "subId": int,
+    "access": boolean,
+    "praise": bigint,
+    "links": string[],
+}
+
+type Practical = BaseCode & {
+
+}
+
+type Project = BaseCode & {
+    "description": string,
+    "techStack": string[],
+}
